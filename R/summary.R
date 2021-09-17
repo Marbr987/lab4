e33620ac3098b53b62c35ffef543cdd474b54005
Marc Braun <marc.y.braun@live.de>
2021-09-17 15:22:20 +0200

    Add Roxygen2 description to summary

--- R/summary.R
+++ R/summary.R
@@ -1,3 +1,8 @@
+#' summary S3 method
+#' @description S3 method for objects of class linreg providing a summary of the object
+#' @param linreg_obj An object of class linreg
+#' @return nothing. Prints a summary of the linreg object.
+#' @export
 summary.linreg <- function(linreg_obj){
   coeff_df <- data.frame("Estimate" = linreg_obj$regression_coefficient, "Std. Error" = sqrt(diag(linreg_obj$variance_of_regression_coefficient)), "t value" = linreg_obj$t_values, "Pr(>|t|)" = linreg$p_values)
   cat('Coefficients:\n')
