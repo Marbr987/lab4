72a59df4a1b4a1c2f6eb5d8780e9a3e73dfe1dd8
Marc Braun <marc.y.braun@live.de>
2021-09-17 16:40:04 +0200

    bugfix

--- R/summary.R
+++ R/summary.R
@@ -4,7 +4,7 @@
 #' @return nothing. Prints a summary of the linreg object.
 #' @export
 summary.linreg <- function(linreg_obj){
-  coeff_df <- data.frame("Estimate" = linreg_obj$regression_coefficient, "Std. Error" = sqrt(diag(linreg_obj$variance_of_regression_coefficient)), "t value" = linreg_obj$t_values, "Pr(>|t|)" = linreg$p_values)
+  coeff_df <- data.frame("Estimate" = linreg_obj$regression_coefficient, "Std. Error" = sqrt(diag(linreg_obj$variance_of_regression_coefficient)), "t value" = linreg_obj$t_values, "Pr(>|t|)" = linreg_obj$p_values)
   cat('Coefficients:\n')
   print(coeff_df)
   cat('---\n\nResidual standard error: ', sqrt(linreg_obj$residuall_variance), 'on', linreg_obj$degree_of_freedom, 'degrees of freedom')
