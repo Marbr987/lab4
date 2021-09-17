881511d2811e8774a0026ae829bb790b955cc592
Marc Braun <marc.y.braun@live.de>
2021-09-17 15:31:50 +0200

    Adapt print method for linreg objects

--- R/print.R
+++ R/print.R
@@ -1,8 +1,11 @@
-print.linreg <- function(QR,y) {
-  y <- round(as.numeric(x$regression_coefficient))
-  names(y) <- names(x$regression_coefficient)
+#' print S3 method
+#' @description S3 method for objects of class linreg printing the call and the coefficients of the linear model
+#' @param linreg_obj An object of class linreg
+#' @return nothing. Prints tha call and the coefficients of the linreg object.
+#' @export
+print.linreg <- function(object) {
   cat("Call:\n")
-  print(x$call)
-  cat("\n\nCoefficients:\n")
-  print(y)
+  print(object$call)
+  cat("\nCoefficients:\n")
+  print(object$regression_coefficient)
 }
