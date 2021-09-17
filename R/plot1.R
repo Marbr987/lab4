799c52e2dcbf4f0aeaf3cdb7018249e609bddf71
Marc Braun <marc.y.braun@live.de>
2021-09-17 16:10:49 +0200

    Added some comments to plot1

--- R/plot1.R
+++ R/plot1.R
@@ -8,15 +8,16 @@
 plot.linreg=function(x,..)
 {
   library(ggplot2)
-  Plot1=data.frame(fitted_values,residuals)
-  p1 <- ggplot(data_frame1,aes(fitted_values,residuals))+
+  Plot1=data.frame(x$fitted_values,x$residuals) # what do you do with Plot1? It is not used anymore... Maybe it should be data_frame1? Also I added the x$ notation
+  p1 <- ggplot(data_frame1,aes(x$fitted_values,x$residuals))+
     geom_point()+geom_abline()+
     title("Residuals vs Fitted")+
     xlab("Fitted values lm(Petal.Length ~ Species)")+
     ylab("Residuals")
   
-  data_frame2 <- data.frame(fitted_values,residuals)
-  p2 <- ggplot(data_frame2,aes(fitted_values,residuals))+
+  # data_frame1 is equal to data_frame2 right? But why is that?
+  data_frame2 <- data.frame(x$fitted_values,x$residuals)
+  p2 <- ggplot(data_frame2,aes(x$fitted_values,x$residuals))+
     geom_point()+geom_abline()+
     ggtitle("Scale-Location")+
     xlab("Fitted values lm(Petal.Length ~ Species)")+
