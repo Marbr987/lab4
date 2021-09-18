8b5f81d450ad79f4e6045b16990185cf2e162681
Marc Braun <marc.y.braun@live.de>
2021-09-18 20:33:51 +0200

    Adjust the first plot to include all necessary parts

--- R/plot3.R
+++ R/plot3.R
@@ -2,16 +2,23 @@ plot.linreg <- function(x, ...){
   library(ggplot2)
   # What data do we need?
   data = data.frame("fitted_values" = x$fitted_values, "residuals" = x$residuals, "sqr_abs_st_residuals" = sqrt(abs(x$residuals / sqrt(x$residual_variance))))
-  ggplot(data=data) +
-    # Which points do we want to plot and how should the data points look like?
-    geom_point(mapping = aes(x = fitted_values, y = residuals), shape = 1, size = 6) +
+  # calculate positions of absolute largest 3 residuals
+  largest_res <- order(abs(data$residuals), decreasing = TRUE)[1:3]
+  # make the actual plot
+  ggplot(data=data, mapping = aes(x = fitted_values, y = residuals)) +
+    # How should the data points look like?
+    geom_point(shape = 1, size = 6) +
     # Background should be white and black borders around it
     theme(panel.border = element_rect(linetype = "solid", fill = NA), panel.background = element_rect(fill = "white", colour = "white"),
           plot.title = element_text(hjust = 0.5)) +
     # Set title
     ggtitle("Residuals vs Fitted") +
-    # Set axis labels
-    labs(x=paste("Fitted values\n", str(x$call), sep=""), y="Residuals") +
+    # Set axis labels including the function used for the regression in a new line
+    labs(x=paste("Fitted values\n linreg(", paste(toString(x$call[2]), ")", sep=""), sep=""), y="Residuals") +
     # Add horizontal dotted line at y = 0.
-    geom_hline(yintercept=0, linetype="dotted", color="grey")
+    geom_hline(yintercept=0, linetype="dotted", color="grey") +
+    # Add red line through the medians
+    stat_summary(fun=median, colour="red", geom="line") +
+    # label the 3 largest residuals
+    geom_text(data = data[largest_res,], mapping = aes(x=fitted_values, y=residuals, label=largest_res), size=4, check_overlap = TRUE, nudge_x = -0.24)
 }
