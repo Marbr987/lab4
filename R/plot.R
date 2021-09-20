f2fe7b67e412374bded5216bb0da39dd01d07957
Marc Braun <marc.y.braun@live.de>
2021-09-20 16:56:46 +0200

    Add ggplot2:: to all functions from the ggplot2 package

--- R/plot.R
+++ R/plot.R
@@ -15,36 +15,36 @@ plot.linreg <- function(x, ...){
   largest_st_res <- order(abs(data$sqr_abs_st_residuals), decreasing = TRUE)[1:3]
   
   # make the actual plots
-  p1 <- ggplot2::ggplot(data=data, mapping = aes(x = fitted_values, y = residuals)) +
+  p1 <- ggplot2::ggplot(data=data, mapping = ggplot2::aes(x = fitted_values, y = residuals)) +
     # How should the data points look like?
-    geom_point(shape = 1, size = 6) +
+    ggplot2::geom_point(shape = 1, size = 6) +
     # Background should be white and black borders around it
-    theme(panel.border = element_rect(linetype = "solid", fill = NA), panel.background = element_rect(fill = "white", colour = "white"),
-          plot.title = element_text(size = 14, hjust = 0.5), axis.text=element_text(size=12), axis.title=element_text(size=14)) +
+    ggplot2::theme(panel.border = ggplot2::element_rect(linetype = "solid", fill = NA), panel.background = ggplot2::element_rect(fill = "white", colour = "white"),
+          plot.title = ggplot2::element_text(size = 14, hjust = 0.5), axis.text=ggplot2::element_text(size=12), axis.title=ggplot2::element_text(size=14)) +
     # Set title
-    ggtitle("Residuals vs Fitted") +
+    ggplot2::ggtitle("Residuals vs Fitted") +
     # Set axis labels including the function used for the regression in a new line
-    labs(x=paste("Fitted values\n linreg(", paste(toString(x$call[2]), ")", sep=""), sep=""), y="Residuals") +
+    ggplot2::labs(x=paste("Fitted values\n linreg(", paste(toString(x$call[2]), ")", sep=""), sep=""), y="Residuals") +
     # Add horizontal dotted line at y = 0.
-    geom_hline(yintercept=0, linetype="dotted", color="grey") +
+    ggplot2::geom_hline(yintercept=0, linetype="dotted", color="grey") +
     # Add red line through the medians
-    stat_summary(fun=median, colour="red", geom="line") +
+    ggplot2::stat_summary(fun=median, colour="red", geom="line") +
     # label the 3 largest residuals
-    geom_text(data = data[largest_res,], mapping = aes(x=fitted_values, y=residuals, label=largest_res), size=4, check_overlap = TRUE, nudge_x = -0.24)
+    ggplot2::geom_text(data = data[largest_res,], mapping = ggplot2::aes(x=fitted_values, y=residuals, label=largest_res), size=4, check_overlap = TRUE, nudge_x = -0.24)
 
-  p2 <- ggplot2::ggplot(data=data, mapping = aes(x = fitted_values, y = sqr_abs_st_residuals)) +
+  p2 <- ggplot2::ggplot(data=data, mapping = ggplot2::aes(x = fitted_values, y = sqr_abs_st_residuals)) +
     # How should the data points look like?
-    geom_point(shape = 1, size = 6) +
+    ggplot2::geom_point(shape = 1, size = 6) +
     # Background should be white and black borders around it
-    theme(panel.border = element_rect(linetype = "solid", fill = NA), panel.background = element_rect(fill = "white", colour = "white"),
-          plot.title = element_text(size = 14, hjust = 0.5), axis.text=element_text(size=12), axis.title=element_text(size=14)) +
+    ggplot2::theme(panel.border = ggplot2::element_rect(linetype = "solid", fill = NA), panel.background = ggplot2::element_rect(fill = "white", colour = "white"),
+          plot.title = ggplot2::element_text(size = 14, hjust = 0.5), axis.text=ggplot2::element_text(size=12), axis.title=ggplot2::element_text(size=14)) +
     # Set title
-    ggtitle("Scale-Location") +
+    ggplot2::ggtitle("Scale-Location") +
     # Set axis labels including the function used for the regression in a new line
-    labs(x=paste("Fitted values\n linreg(", paste(toString(x$call[2]), ")", sep=""), sep=""), y=expression(sqrt(abs("Standardized Residuals")))) +
+    ggplot2::labs(x=paste("Fitted values\n linreg(", paste(toString(x$call[2]), ")", sep=""), sep=""), y=expression(sqrt(abs("Standardized Residuals")))) +
     # Add red line through the medians
-    stat_summary(fun=median, colour="red", geom="line") +
+    ggplot2::stat_summary(fun=median, colour="red", geom="line") +
     # label the 3 largest residuals
-    geom_text(data = data[largest_st_res,], mapping = aes(x=fitted_values, y=sqr_abs_st_residuals, label=largest_st_res), size=4, check_overlap = TRUE, nudge_x = -0.24)
+    ggplot2::geom_text(data = data[largest_st_res,], mapping = ggplot2::aes(x=fitted_values, y=sqr_abs_st_residuals, label=largest_st_res), size=4, check_overlap = TRUE, nudge_x = -0.24)
   return(list(p1, p2))
 }
